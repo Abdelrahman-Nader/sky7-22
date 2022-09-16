85764d0af3af4ae2548cfbb9c83f7bf578a4e697
Abdelrahaman Nader <abualiya.2202@gmail.com>
2022-09-16 17:19:35 +0200

    fix issue in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -24,7 +24,6 @@ import { ProductsComponent } from './compants/products/products.component';
   declarations: [
     NavBarComponent,
     AppComponent,
-    Component,
     HomeComponent,
     OurUsComponent,
     CallUsComponent,
