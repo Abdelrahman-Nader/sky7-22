aa4ecf35951efc16a5cc983c9db3460a4beb6605
Abdelrahaman Nader <abualiya.2202@gmail.com>
2022-10-27 18:42:50 +0200

    test

---
+++ src/app/products/model products/category.ts
@@ -0,0 +1,8 @@
+export interface Category {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+  imgurl?: string;
+  categoryID: number;
+}
---
+++ src/app/products/model products/prouduct-basic.ts
@@ -0,0 +1,8 @@
+export interface ProuductBasic {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+  imgurl?: string;
+  categoryID: number;
+}
