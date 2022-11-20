a43a58b16e2044bf11971943b4e823dcc22216fe
Abdelrahaman Nader <abualiya.2202@gmail.com>
2022-11-20 17:48:52 +0200

    comite directive

--- src/app/Dirctive/style.directive.ts
+++ src/app/Dirctive/style.directive.ts
@@ -1,22 +1,30 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
-export class StyleDirective {
+export class StyleDirective implements OnChanges {
+
+  @Input() defColor: string = "#2c3e50";
+  @Input() highColor: string = "#1abc9c";
+
 
   constructor(private elementRef: ElementRef) {
     // this.elementRef.nativeElement.style.border = "3px solid red"
   }
+  ngOnChanges(): void {
+    this.defColor = "#2c3e50";
+  }
+  
 
 
   @HostListener('mouseenter') onMouseEnter() {
 
-    this.elementRef.nativeElement.style.border = "3px solid red"
+    this.elementRef.nativeElement.style.border =`3px solid ${this.defColor}`
   }
   @HostListener('mouseleave') onMouseLeave() {
 
-    this.elementRef.nativeElement.style.border = "2px solid black"
+    this.elementRef.nativeElement.style.border = `2px solid ${this.highColor}`
   }
 
 
